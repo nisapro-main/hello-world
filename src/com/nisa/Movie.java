3652536332c2b0b99a194aafb1edfb22c17f3715
nisapro-main <nisa_productions@yahoo.com>
2015-02-02 16:18:18 -0500

    Added hashcode and equals methods.

--- src/com/nisa/Movie.java
+++ src/com/nisa/Movie.java
@@ -53,6 +53,44 @@ public class Movie {
             return String.format("Movie: Name=%s, Director=%s, Release date=%s.",
                     name, director, FORMAT.format(year) );
         }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((director == null) ? 0 : director.hashCode());
+            result = prime * result + ((name == null) ? 0 : name.hashCode());
+            result = prime * result + ((year == null) ? 0 : year.hashCode());
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            MovieDTO other = (MovieDTO) obj;
+            if (director == null) {
+                if (other.director != null)
+                    return false;
+            } else if (!director.equals(other.director))
+                return false;
+            if (name == null) {
+                if (other.name != null)
+                    return false;
+            } else if (!name.equals(other.name))
+                return false;
+            if (year == null) {
+                if (other.year != null)
+                    return false;
+            } else if (!year.equals(other.year))
+                return false;
+            return true;
+        }
+        
     }
     
     public Movie() {
